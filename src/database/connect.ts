6ca458a949c482fb7388f13841a11e2b912203a7
Igor Mont <igornathandev@gmail.com>
2022-01-14 16:06:31 -0300

    config typeorm

---
+++ src/database/connect.ts
@@ -0,0 +1,12 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async(host = "db_auth"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+  console.log("📦 Connected with database");
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host
+    })
+  )
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import "reflect-metadata";
 import express from "express";
+
+import connection from "./database/connect";
 import { routes } from './routes';
 
 const app = express();
+
+connection();
 
 app.use(express.json());
 app.use(routes);
